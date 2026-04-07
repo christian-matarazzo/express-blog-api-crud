b22d0e2ca1d3d180adfcbd25dce2f6cfba4dba20
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-04-07 11:46:46 +0200

    add CRUD

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const postsRouter = require('./routes/posts') /* importing routing */
 
+
+app.use('static', express.static('public'))
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server del mio blog')
 })
-
+app.use('/posts', postsRouter)
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
