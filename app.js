1fe1db49f7d5cdb2a4686b2e803796f7686182ed
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-04-09 12:34:38 +0200

    add notFound middleware

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 const postsRouter = require('./routes/posts') /* importing routing */
-
 app.use('static', express.static('public'))
+
+const notFound = require("./middlewares/notFound")
+
+
+
 app.use(express.json()) /* body parser */
+
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
+
 app.use('/posts', postsRouter)
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
+
+app.use(notFound)
